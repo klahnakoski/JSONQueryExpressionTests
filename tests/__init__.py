7bed6cbc23b7e5412d2c89a9f997f54e53abd05f
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-01-07 14:19:05 -0500

    first commit from ActievData tests directory

---
+++ tests/__init__.py
@@ -0,0 +1,17 @@
+from pyDots import listwrap
+from pyLibrary.queries.expressions import NullOp
+
+NULL = NullOp()
+
+def parametrize(names, values):
+    def output(func):
+        def p_func(self, *args, **kwargs):
+            for i, v in enumerate(values):
+                n = listwrap(names)
+                v = listwrap(v)
+
+                func(self, **{nn: vv for nn, vv in zip(n, v)})
+
+        return p_func
+
+    return output
